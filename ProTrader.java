f0024ab48a65bdac44a140b0ee8a19f3fee43142
Zachary Daly <zdaly3183@gmail.com>
2018-11-01 10:42:50 -0700

    started pro trader

---
+++ ProTrader.java
@@ -0,0 +1,13 @@
+public class ProTrader{
+   
+   public static void main(String[] Args){
+      
+      //make characters and their item lists
+      //make shop
+      //make list of item modifiers "gloss, glue, etc."
+      
+   }
+   
+   //public static void 
+   
+}
